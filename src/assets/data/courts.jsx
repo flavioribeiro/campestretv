6b481c17bf1a1f8067f023208ea684713d7cb47d
Flavio Ribeiro <flavior@netflix.com>
2025-11-22 18:33:28 -0300

    bugfix on selecting current court

--- src/assets/data/courts.jsx
+++ src/assets/data/courts.jsx
@@ -1,23 +1,18 @@
 const courts = [
   {
-    id: "1",
+    id: 5,
     url: "https://img.youtube.com/vi/qKxrODi2uBo/maxresdefault.jpg",
     thumb: "https://img.youtube.com/vi/qKxrODi2uBo/maxresdefault.jpg",
   },
   {
-    id: "2",
+    id: 6,
     url: "https://img.youtube.com/vi/pEPdLJhq6-k/maxresdefault.jpg",
     thumb: "https://img.youtube.com/vi/pEPdLJhq6-k/maxresdefault.jpg",
   },
   {
-    id: "3",
+    id: 8,
     url: "https://img.youtube.com/vi/Q73DDD7rrfY/maxresdefault.jpg",
     thumb: "https://img.youtube.com/vi/Q73DDD7rrfY/maxresdefault.jpg",
-  },
-  {
-    id: "4",
-    url: "https://img.youtube.com/vi/vMAnXYRtIns/maxresdefault.jpg",
-    thumb: "https://img.youtube.com/vi/vMAnXYRtIns/maxresdefault.jpg",
   },
 ];
 
--- src/components/content.jsx
+++ src/components/content.jsx
@@ -5,8 +5,17 @@ import courts from "../assets/data/courts";
 import ads from "../assets/img/ads-img.jpg";
 
 const Content = ({ props }) => {
-  const currentCourtId = props - 1;
-  const currentCourt = courts[currentCourtId];
+  console.log(props, courts);
+  const currentCourtId = props;
+  let currentCourt = null;
+  for (const court of courts) {
+    console.log("checking court", court.id, currentCourtId);
+    if (court.id === currentCourtId) {
+      console.log("found current court", court);
+      currentCourt = court;
+      break;
+    }
+  }
 
   return (
     <div className="main-content">
