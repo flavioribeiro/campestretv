7e102abbab76a895a5d6868e34add5a421f5b302
Flavio Ribeiro <flavior@netflix.com>
2025-12-01 14:06:53 -0300

    link youtube urls to court 5 and 6

--- src/assets/data/courts.jsx
+++ src/assets/data/courts.jsx
@@ -1,18 +1,13 @@
 const courts = [
   {
     id: 5,
-    url: "https://www.youtube.com/embed/cFo9Wq0wCDo",
-    thumb: "https://i.ytimg.com/vi/cFo9Wq0wCDo/maxresdefault.jpg"
+    url: "https://www.youtube.com/embed/3JUB0YpSWjc",
+    thumb: "https://i.ytimg.com/vi/3JUB0YpSWjc/maxresdefault.jpg"
   },
   {
     id: 6,
-    url: "https://www.youtube.com/embed/cFo9Wq0wCDo",
-    thumb: "https://i.ytimg.com/vi/cFo9Wq0wCDo/maxresdefault.jpg"
-  },
-  {
-    id: 8,
-    url: "https://www.youtube.com/embed/cFo9Wq0wCDo",
-    thumb: "https://i.ytimg.com/vi/cFo9Wq0wCDo/maxresdefault.jpg"
+    url: "https://www.youtube.com/embed/_qGzhEyIx4E",
+    thumb: "https://i.ytimg.com/vi/_qGzhEyIx4E/maxresdefault.jpg"
   },
 ];
 
