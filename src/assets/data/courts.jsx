fd7b0a9fd2f41006227a028449488eae36d20129
Flavio Ribeiro <flavior@netflix.com>
2025-12-03 13:36:37 -0300

    update with youtube links for wednesday

--- src/assets/data/courts.jsx
+++ src/assets/data/courts.jsx
@@ -1,13 +1,13 @@
 const courts = [
   {
     id: 5,
-    url: "https://www.youtube.com/embed/TYcvj0oqmiQ",
-    thumb: "https://i.ytimg.com/vi/TYcvj0oqmiQ/maxresdefault.jpg"
+    url: "https://www.youtube.com/embed/bDEDVVywOrw?si=umQMZ-f7hGRroxGE",
+    thumb: "https://i.ytimg.com/vi/bDEDVVywOrw/maxresdefault.jpg"
   },
   {
     id: 6,
-    url: "https://www.youtube.com/embed/_qGzhEyIx4E",
-    thumb: "https://i.ytimg.com/vi/_qGzhEyIx4E/maxresdefault.jpg"
+    url: "https://www.youtube.com/embed/K1ro01GY2ac?si=xAyUWo5glkD-RZhF",
+    thumb: "https://i.ytimg.com/vi/K1ro01GY2ac/maxresdefault.jpg"
   },
 ];
 
