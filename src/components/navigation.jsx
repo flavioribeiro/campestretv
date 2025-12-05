30d277edc613cb2f1412a3b17456982ed3f0a96a
Flavio Ribeiro <flavior@netflix.com>
2025-12-04 22:28:03 -0300

    minor refactor on navigation bar

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -3,13 +3,19 @@ import { NavLink } from "react-router-dom";
 import courts from "../assets/data/courts";
 
 const Navigation = () => {
+  const getThumbnailUrl = (court) => {
+    if (court.type === "youtube") {
+      return `https://i.ytimg.com/vi/${court.videoId}/maxresdefault.jpg`;
+    }
+    return `https://img.youtube.com/vi/pEPdLJhq6-k/maxresdefault.jpg`;
+  }
   return (
     <nav className="navigation">
       <ul>
         {courts.sort((a, b) => a.id - b.id).map((court) => (
           <li key={court.id}>
             <NavLink to={`/quadra/${court.id}`}>
-              {court.type === "youtube" ? <img src={`https://i.ytimg.com/vi/${court.videoId}/maxresdefault.jpg`} /> : <img src={court.thumb} />}
+              <img src={getThumbnailUrl(court)} />
               <h3>Quadra {court.id}</h3>
             </NavLink>
           </li>
